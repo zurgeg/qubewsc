a572e9411ef77bc161dfcff483dbd4ccc28833db
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-05 11:35:23 -0400

    Alright so we need to extract the data from the WAD

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ def generate_listings():
     os.chdir("titles/")
     tmds = []
     is_first = True
-    for file in glob.glob("*tmd*"):
+    for file in glob.glob("*.wad"):
         if is_first:
             is_first = False
             click.echo(click.style(f"Creating listing for titles/{file}", fg="green"))
         else:
-            click.echo(click.style(f"\033[FCreating listing for titles/{file}", fg="green"), nl=False)
+            click.echo(click.style(f"{colorama.ansi.AnsiCursor.UP(1)}Creating listing for titles/{file}", fg="green"), nl=False)
         
 
 if __name__ == "__main__":
